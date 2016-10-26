c7b51dfd998593cc50c8240eeef06e456bd29c8a
yilongchun <5115413@qq.com>
2016-10-26 15:20:28 +0800

    123

--- jqdl/jqdl/ArchitectWorld/js/radar.js
+++ jqdl/jqdl/ArchitectWorld/js/radar.js
@@ -10,10 +10,10 @@
 			AR.radar.container = document.getElementById("radarContainer");
 
 			// set the back-ground image for the radar
-			AR.radar.background = new AR.ImageResource("assets/radar_bg2.png");
+			AR.radar.background = new AR.ImageResource("assets/radar_bg.png");
 
 			// set the north-indicator image for the radar (not necessary if you don't want to display a north-indicator)
-//			AR.radar.northIndicator.image = new AR.ImageResource("assets/radar_north.png");
+			AR.radar.northIndicator.image = new AR.ImageResource("assets/radar_north.png");
 
 			// center of north indicator and radar-points in the radar asset, usually center of radar is in the exact middle of the bakground, meaning 50% X and 50% Y axis --> 0.5 for centerX/centerY
 			AR.radar.centerX = 0.5;
